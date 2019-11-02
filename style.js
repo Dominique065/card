e271c66ec7889da36ae34820900d140cf9ffb8a8
Dominika Korbas <misia@Air-Dominika.home>
2019-11-02 16:22:31 +0100

    Zarys układu strony

---
+++ style.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      var menuHeight = document.getElementById('idMenu').clientHeight
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - menuHeight - 10
+      }, 800, function () {
+        window.location.hash = hash - menuHeight - 10;
+      });
+    } // End if
+  });
+
+
+});
+
+$(function () {
+  $('.menuButton').on('click', function () {
+    $('.menu ul').toggleClass('open');
+  });
+});
+
